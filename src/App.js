c0238b178bec8cb992174d705d3e225fb39cb838
Kat mcg <cathaumann@gmail.com>
2020-07-28 15:32:34 +0200

    fix

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Hello horny!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
